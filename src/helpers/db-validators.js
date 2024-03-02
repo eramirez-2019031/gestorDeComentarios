43cc475e631833c1863798c943b57cb8e1f7f1c3
eramirez-2019031 <eramirez-2019031@kinal.edu.gt>
2024-03-02 12:13:39 -0600

    ya cree el model y el controlador de usuarios

---
+++ src/helpers/db-validators.js
@@ -0,0 +1,15 @@
+import Usuario from '../usuario/user.model.js';
+
+export const existenteEmail = async (email = '') => {
+    const existeEmail = await Usuario.findOne({email});
+    if(existeEmail){
+        throw new Error(`El email ${ email } ya fue registrado`);
+    }
+}
+
+export const existeUsuarioById = async ( id = '') => {
+    const existeUsuario = await Usuario.findOne({id});
+    if(existeUsuario){
+        throw new Error(`El usuario con el id: ${ id } no existe`);
+    }
+}
---
+++ src/usuario/user.controller.js
@@ -0,0 +1,117 @@
+import { response, request } from  'express';
+import bcryptjs from 'bcryptjs';
+import User from './user.model.js';
+
+export const userPut = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { _id, password, ...resto } = req.body;
+
+        const usuarioAutenticado = req.usuario;
+        const idCoincide = usuarioAutenticado._id.toString() === id;
+        const tienePermiso = usuarioAutenticado.role === 'USER_ROLE';
+
+        if (!idCoincide || !tienePermiso) {
+            return res.status(403).json({
+                msg: 'No tienes permiso para actualizar este usuario',
+            });
+        }
+
+        
+        if (!password) {
+            return res.status(400).json({
+                msg: 'Debes proporcionar la contraseña anterior para actualizar',
+            });
+        }
+
+        
+        const usuario = await User.findById(id);
+        if (!usuario) {
+            return res.status(400).json({
+                msg: 'Usuario no encontrado',
+            });
+        }
+
+        const contrasenaValida = await bcryptjs.compare(password, usuario.password);
+        if (!contrasenaValida) {
+            return res.status(400).json({
+                msg: 'La contraseña anterior no es válida',
+            });
+        }
+
+        const updatedUser = await User.findByIdAndUpdate(id, resto, { new: true });
+        
+        res.status(200).json({
+            msg: 'Se actualizó el perfil correctamente',
+            usuario: updatedUser,
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            msg: 'Hable con el administrador',
+        });
+    }
+}
+
+
+export const registrarse = async (req, res) => {
+    const { nameUser, email, password, role } = req.body;
+    const usuario = new User({ nameUser, email, password, role });
+
+    const salt = bcryptjs.genSaltSync();
+    usuario.password = bcryptjs.hashSync(password, salt);
+
+    await usuario.save();
+    res.status(200).json({
+        usuario
+    });
+}
+
+const login = async (req, res) => {
+    
+    const { email, password } = req.body;
+    
+    try {
+        
+        const usuario = await User.findOne({ email });
+
+
+        if (!usuario) {
+            return res.status(400).json({
+                msg: 'El correo no está registrado'
+            });
+        }
+        
+
+        if (!usuario.estado) {
+            return res.status(400).json({
+                msg: 'El usuario no existe'
+            });
+        }
+
+        const validPassword = bcryptjs.compareSync(password, usuario.password);
+        if (!validPassword) {
+            return res.status(400).json({
+                msg: 'La contraseña no coincide'
+            });
+        }
+        
+
+        const token = await generarJWT(usuario.id);
+
+
+        res.status(200).json({
+            msg: 'Se inicio secion',
+            usuario,
+            token
+        });
+
+
+    } catch (error) {
+        console.log("Error:", error);
+        res.status(500).json({
+            msg: 'Error, hablele al admin'
+        });
+    }
+}
+
