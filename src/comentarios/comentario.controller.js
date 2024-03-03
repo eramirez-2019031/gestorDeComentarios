aedb7a31395709adcebce8444086c0b8ab96a024
eramirez-2019031 <eramirez-2019031@kinal.edu.gt>
2024-03-03 12:10:47 -0600

    estoy haciendo el controlador de comentarios

---
+++ src/comentarios/comentario.controller.js
@@ -0,0 +1,111 @@
+import comentario from  './comentario.model';
+const userHasComment = require('../models/userHasComment');
+
+
+export const crearPublicacion = async (req, res) => {
+    try {
+        const { titulo, categoria, descripcion } = req.body;
+        const comentario = new Comentario({
+            titulo,
+            categoria,
+            descripcion,
+            user: req.usuario._id
+        });
+        await comentario.save();
+        res.status(201).json({
+            msg: 'Se publicó la publicación',
+            comentario
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            msg: 'Hable con el administrador',
+        });
+    }
+};
+
+export const putPublicacion = async (req, res) => {
+    try {
+        const { fecha } = req.params; 
+        const { titulo, categoria, descripcion } = req.body;
+
+        const comentario = await Comentario.findOne({ fecha }); // va a buscar la publicacion por fecha  en vez de ID para evitar conflictos
+        if (!comentario) {
+            return res.status(404).json({
+                msg: 'La publicacion no se encontro',
+            });
+        }
+        if (comentario.user.toString() !== req.usuario._id.toString()) {
+            return res.status(403).json({
+                msg: 'No puedes editar esta publicación',
+            });
+        }
+
+        comentario.titulo = titulo;
+        comentario.categoria = categoria;
+        comentario.descripcion = descripcion;
+        await comentario.save();
+
+        res.status(200).json({
+            msg: 'La Publicación fue editada con exito',
+            comentario
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            msg: 'comuniquese con el admin',
+        });
+    }
+};
+
+export const addComment = async (req, res) => {
+    try {
+        const { userId } = req.params;
+        const { comentarioId, titulo, descripcion } = req.body; 
+
+        const nuevoComentario = new usercomentario({
+            user: userId,
+            comentario: comentarioId,
+            titulo: titulo, 
+            descripcion: descripcion 
+        });
+
+        await nuevoComentario.save(); 
+
+        res.status(200).json({
+            msg: 'Comentario enviado',
+            comentario: nuevoComentario, 
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            msg: 'Hable con el administrador',
+        });
+    }
+};
+
+export const getComentarioComments = async (req, res) => {
+    try {
+        // Buscar todos los comentarios
+        const comentarios = await Comentario.find();
+
+        // Para cada comentario, encontrar los comentarios asociados
+        for (let comentario of comentarios) {
+            // Buscar comentarios asociados al comentario actual
+            const comments = await usercomentario.find({ comentario: comentario._id });
+            comentario.comments = comments; 
+        }
+
+        res.status(200).json({
+            msg: 'publicaciones encontradas con sus comentarios',
+            comentarios,
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            msg: 'error, hable con el admin',
+        });
+    }
+};
+
+
---
+++ src/comentarios/comentario.model.js
@@ -0,0 +1,35 @@
+import mongoose from 'mongoose';
+
+const comentarioSchema = new mongoose.Schema({
+    titulo: {
+        type: String,
+        required: [true, 'El titulo es obligatorio']
+    },
+    categoria: {
+        type: String,
+        required: [true, 'Se necesita una Categoria']
+    },
+    descripcion: {
+        type: String,
+        required: [true, 'No puede publicar sin texto']
+    },
+    user: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }],
+    estado: {
+        type: Boolean,
+        default: true
+    },
+    fecha: {
+        type: Date,
+        default: Date.now
+    }
+});
+comentarioSchema.methods.toJSON = function () {
+    const { __v, _id, createdAt, ...comentario } = this.toObject();
+    comentario.pid = createdAt; // Utilizamos la fecha de creación como identificador
+    return comentario;
+}
+
+ export default mongoose.model('comentario', comentarioSchema);
